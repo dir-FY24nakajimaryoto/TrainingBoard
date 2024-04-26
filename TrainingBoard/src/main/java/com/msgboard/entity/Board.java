f6d40899a9e982a656560b76bb4043d610791ed5
dir-FY24nakajimaryoto <ryoto.nakajima.m632@dirbato.co.jp>
2024-04-26 12:25:37 +0900

    Boardエンティティクラスの作成

--- TrainingBoard/src/main/java/com/msgboard/entity/Board.java
+++ TrainingBoard/src/main/java/com/msgboard/entity/Board.java
@@ -1,21 +1,51 @@
 package com.msgboard.entity;
 
+import java.util.Date;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
-@Data
+
 @Entity
+@Data
 @Table(name="default")
 public class Board {
 	
+	/**
+	 * 主キー
+	 */
 	@Id
 	@Column
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
+	
+	/**
+	 * スレッド設立時刻の自動入力
+	 */
+	@Column
+	private Date createDate;
+	
+	/**
+	 * スレッドタイトル(要修正.後にフォーマットに含める)
+	 */
+	@Column(nullable = false)
+	@NotEmpty(message = "タイトルを入力してください")
+	@Size(max = 100, message = "タイトルは100字以下で入力してください")
+	private String title;
+	
+	/**
+	 * 質問内容
+	 */
+	@Column(nullable = false)
+	@NotEmpty(message = "内容を入力してください")
+	private String contents;
+		
 	
 }
