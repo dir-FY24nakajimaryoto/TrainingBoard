1e6e7152c5c31bbc30c194df6d42a09fb854bd52
dir-FY24nakajimaryoto <ryoto.nakajima.m632@dirbato.co.jp>
2024-04-30 15:27:45 +0900

    レッスン番号の選択機能を追加（作成/更新時）

--- TrainingBoard/src/main/java/com/msgboard/entity/Board.java
+++ TrainingBoard/src/main/java/com/msgboard/entity/Board.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -21,6 +23,35 @@ import lombok.Data;
 @Table(name="board")
 public class Board {
 	
+	/** レッスンNo.の列挙型 */
+	public static enum Lesson{
+		L0_初期設定,
+		L1_学習概要,
+		L2_Java基礎,
+		L3_オブジェクト指向,
+		L4_継承,
+		L5_コレクション,
+		L6_テスト,
+		L7_GitHub,
+		L8_データベース,
+		L9_JDBC基礎,
+		L10_JDBC応用,
+		L11_Hibernate,
+		L12_インターネット基礎,
+		L13_HTML基礎,
+		L14_CSS基礎,
+		L15_サーブレット基礎,
+		L16_サーブレット応用,
+		L17_Web開発基礎,
+		L18_Web開発応用,
+		L19_SpringBoot,
+		L20_Tymeleaf,
+		L21_SpringBoot実習,
+		L22_AWSとシステムのデプロイ,
+		その他
+	}
+	
+	
 	/** 主キー */
 	@Id
 	@Column(nullable = false)
@@ -35,6 +66,12 @@ public class Board {
 	/** スレッド更新時刻の自動入力 */
 	@Column
 	private Date updateDate;
+	
+	
+	/** Lesson No. */
+	@Column
+	@Enumerated(EnumType.STRING)
+	private Lesson lesson;
 	
 	
 	/** スレッドタイトル(要修正.後にフォーマットに含める) */
